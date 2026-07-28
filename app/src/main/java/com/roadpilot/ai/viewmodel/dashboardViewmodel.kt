c86291cc721bfba59b29d04857cc27c700d37f63
anuraanj <anuranj220@gmail.com>
2026-07-29 02:28:20 +0530

    MVVM foundation and project structure

---
+++ app/src/main/java/com/roadpilot/ai/viewmodel/dashboardViewmodel.kt
@@ -0,0 +1,16 @@
+package com.roadpilot.ai.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    var status by mutableStateOf("Ready")
+        private set
+
+    fun updateStatus(newStatus: String) {
+        status = newStatus
+    }
+}
